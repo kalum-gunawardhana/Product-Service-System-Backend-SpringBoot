bdb2ddf6640ec04eac1e89d0c65ccd7021f9c4bf
Kalum Gunawardhana <kguna415@gmail.com>
2025-04-28 14:56:58 +0530

    first commit

---
+++ src/test/java/edu/product_service/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.product_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
